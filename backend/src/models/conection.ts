07698392643d1ec0a8654b767f01856bec39b675
Aphinan <154401387+shokun580@users.noreply.github.com>
2025-10-21 10:58:57 +0700

    fix: update default database name to 'my_oom'

--- backend/src/models/conection.ts
+++ backend/src/models/conection.ts
@@ -4,7 +4,7 @@ const config:PoolOptions = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'webdb',
+    database: process.env.DB_NAME || 'my_oom',
     port: Number(process.env.DB_PORT || 3306)
 }
 
